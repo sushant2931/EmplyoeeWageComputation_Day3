42656226cd12c7a96839513078d446fd900858a4
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 13:11:08 +0530

    REFACTORED: Monthly Wage Calculation Using Switch Case

--- src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
@@ -4,28 +4,34 @@ public class EmployeeWageComputation {
     private static final int IS_FULL_TIME = 1;
     private static final int IS_PART_TIME = 2;
     private static final int EMP_WAGE_PER_HOUR = 20;
+    private static final int WORKING_DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
-
-        int empHrs, empWage;
-        int empType = (int)Math.floor(Math.random() * 10) % 3;
         System.out.println("----- Welcome to Employee Wage Computation! -----");
 
-        switch(empType) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
-                break;
+        int empHrs = 0, empWagePerDay = 0, empWagePerMonth = 0;
+        for(int day = 0 ; day<=WORKING_DAYS_PER_MONTH ; day++) {
+
+            int empType = (int)Math.floor(Math.random() * 10) % 3;//typecasting
+
+            switch(empType) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+
+            empWagePerDay = empHrs * EMP_WAGE_PER_HOUR;
+            empWagePerMonth += empWagePerDay;
+            System.out.println("Emp Hours : "+empHrs);
+            System.out.println("Emp Wage Per Day : "+empWagePerDay);
         }
 
-        empWage = empHrs * EMP_WAGE_PER_HOUR;
-        System.out.println("Emp Hours : "+empHrs);
-        System.out.println("Emp Wage : "+empWage);
+        System.out.println("Emp Wage Per Month : "+empWagePerMonth);
     }
 }
 
