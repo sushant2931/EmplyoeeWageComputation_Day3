f15cd9ecb60af607c6d5ca83e719e3e78bc9818d
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 12:56:46 +0530

    Added :Check Emplyoee Attendance Present Or Absent

--- src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
@@ -3,5 +3,12 @@ package com.bridegelabz.emplyoeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("......Welcome On Employee Wage Computation Program.....");
+        int empPresesent = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == empPresesent){
+            System.out.println("Employee is Present");
+        }
+        else
+            System.out.println("Employee is Absent");
     }
 }
