d0680810977e896a15bb3058bc72b8203b387ebd
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 13:07:43 +0530

    REFACTORED :Using Switch Case

--- src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
@@ -1,23 +1,31 @@
 package com.bridegelabz.emplyoeewagecomputation;
 
 public class EmployeeWageComputation {
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    private static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        System.out.println("......Welcome On Employee Wage Computation Program.....");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_WAGE_PER_HOUR = 20;
 
         int empHrs, empWage;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 4;
-        else
-            empHrs = 0;
+        int empType = (int)Math.floor(Math.random() * 10) % 3;
+        System.out.println("----- Welcome to Employee Wage Computation! -----");
+
+        switch(empType) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+                break;
+        }
 
         empWage = empHrs * EMP_WAGE_PER_HOUR;
         System.out.println("Emp Hours : "+empHrs);
         System.out.println("Emp Wage : "+empWage);
     }
 }
+
