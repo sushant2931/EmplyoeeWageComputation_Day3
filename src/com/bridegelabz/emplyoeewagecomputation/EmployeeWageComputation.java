202b5e41066ba59c509973da062377d4a6d90bd6
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 13:00:19 +0530

    REFACTORED: Check Daily Emplyoee  Wage

--- src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
+++ src/com/bridegelabz/emplyoeewagecomputation/EmployeeWageComputation.java
@@ -3,12 +3,18 @@ package com.bridegelabz.emplyoeewagecomputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("......Welcome On Employee Wage Computation Program.....");
-        int empPresesent = 1;
+        int IS_PRESENT = 1;
+        int EMP_WAGE_PER_HOUR = 20;
+
+        int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == empPresesent){
-            System.out.println("Employee is Present");
-        }
-        else
-            System.out.println("Employee is Absent");
+        if (empCheck == IS_PRESENT) {
+            empHrs = 8;
+        } else
+            empHrs = 0;
+
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Hours : " + empHrs);
+        System.out.println("Emp Wage : " + empWage);
     }
 }
